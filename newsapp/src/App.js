a94becb1b35c3c462ba36aadca6a21c492c1fe29
FraNzY <anoj1810@gmail.com>
2023-06-02 13:48:34 +0545

    creating newsapp with class based components

--- newsapp/src/App.js
+++ newsapp/src/App.js
@@ -4,10 +4,11 @@ import './App.css';
 import React, { Component } from 'react'
 
 export default class App extends Component {
+   a= "anoj"
   render() {
     return (
       <div>
-        Hello my first class based component
+        Hello my first class based component {this.a}
       </div>
     )
   }
