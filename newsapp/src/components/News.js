74b28e1312fae7d1c544459eba00dc2c538c839c
FraNzY <anoj1810@gmail.com>
2023-06-03 18:29:23 +0545

    calling newsapi and building top headlines

--- newsapp/src/components/News.js
+++ newsapp/src/components/News.js
@@ -3,12 +3,19 @@ import NewsItem from './NewsItem'
 
 export default function News() {
   return (
-    <div>
-      This is a news component
-      <NewsItem />
-      <NewsItem />
-      <NewsItem />
-      <NewsItem />
+    <div className='container my-3'>
+      <h2>NewsMonkey - Top Headlines</h2>
+       <div className="row">
+        <div className="col-md-4">
+            <NewsItem title = "pic1" description ="this is pic 1"/>
+        </div>
+        <div className="col-md-4">
+            <NewsItem title = "pic1" description ="this is pic 1"/>
+        </div>
+        <div className="col-md-4">
+            <NewsItem title = "pic1" description ="this is pic 1"/>
+        </div>
+      </div>
     </div>
   )
 }
